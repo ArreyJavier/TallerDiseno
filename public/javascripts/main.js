6ff0fe4b7d022790189a38c5e1d3c731b639c8f6
ArreyJavier <javier.arrey.13@sansano.usm.cl>
2018-11-05 11:33:44 -0300

    AdminLogin y UserCRUD v2.0
    Falta crear el login normal (no con firebase-ui) y la opcion editar.

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -11,28 +11,79 @@ if (!firebase.apps.length) {
     firebase.initializeApp(config);
 }
 
+if(firebase.apps.length === 1){
+  var secondaryApp = firebase.initializeApp(config, "create");
+  var deleteApp = firebase.initializeApp(config, "delete");
+}
+
 firebase.auth().onAuthStateChanged(function(user) {
-    AdminPermissions(user);
     if (user) {
         uid = user.uid;
         uemail = user.email;
+        if (uemail === "admin@tallerd.com"){
+          var createUser = document.getElementById("myDIV");
+          createUser.style.display = "block";
+        }
     } else {
         window.location.href="/";
     }
 });
 
-function AdminPermissions(user) {
-  if(user.email === "admin@tallerd.com"){
-    var createUser = document.getElementById("myDIV");
-    createUser.style.display = "block";
-    document.getElementById("createUser").addEventListener('click', function(event) {
-      var secondaryApp = firebase.initializeApp(config, "Secondary");
-      const newUEmail = document.getElementById('correo-input').value;
-      const newUPassword = document.getElementById('contraseña-input').value;
-      secondaryApp.auth().createUserWithEmailAndPassword(newUEmail, newUPassword);
-      secondaryApp.auth().signOut();
-      document.getElementById('correo-input').value = "";
-      document.getElementById('contraseña-input').value = "";
+document.getElementById("createUser").addEventListener('click', function(event) {
+  const newUEmail = document.getElementById('correo-input').value;
+  const newUPassword = document.getElementById('contraseña-input').value;
+  secondaryApp.auth().createUserWithEmailAndPassword(newUEmail, newUPassword).then(function(value) {
+    var user = secondaryApp.auth().currentUser;
+    var dbUsuarios = firebase.database().ref("Usuarios/");
+    dbUsuarios.child(user.uid).set({correo: newUEmail, contraseña: newUPassword})
+    }, function(error) {
+  });
+  secondaryApp.auth().signOut();
+  document.getElementById('correo-input').value = "";
+  document.getElementById('contraseña-input').value = "";
+  renderUsuarios();
+});
+
+
+var dbUsuarios = firebase.database().ref("Usuarios/");
+var usuarios = [];
+dbUsuarios.on("child_added", function(snapshot){
+  var data = snapshot.val();
+  usuario = {
+      "correo": data.correo,
+      "contraseña": data.contraseña
+  };
+  usuarios.push(usuario);
+  renderUsuarios();
+});
+
+function deleteUsuario(index){
+    deleteApp.auth().signInWithEmailAndPassword(usuarios[index].correo,usuarios[index].contraseña).then(function(value) {
+      var user = deleteApp.auth().currentUser;
+      var dbUsuarios = firebase.database().ref("Usuarios/"+user.uid);
+      dbUsuarios.remove();
+      user.delete();
     });
-  }
+    deleteApp.auth().signOut();
+    usuarios.splice(index, 1);
+    renderUsuarios();
+}
+
+function appendUsuario(usuario, index){
+    document.getElementById('usuarios').innerHTML += `
+                        <tr id="usuario${index}">
+                            <td> ${usuario.correo} </td>
+                            <td> ${usuario.contraseña} </td>
+                            <td class="text-right">
+                                <div class="btn-group">
+                                    <button onclick="initEditUsuario(${index})" class="btn-white btn btn-xs">Editar</button>
+                                    <button onclick="deleteUsuario(${index})" class="btn-danger btn btn-xs">Delete</button>
+                                </div>
+                            </td>
+                        </tr>`;
+}
+
+function renderUsuarios(){
+    document.getElementById('usuarios').innerHTML = '';
+    usuarios.forEach((usuario, i) => appendUsuario(usuario, i));
 }
